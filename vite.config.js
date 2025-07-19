0243912f53e93a14594b0be5053a749cba367310
Jean <jean87080981@gmail.com>
2025-07-19 19:29:17 +0800

    Update vite.config.js for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? (process.env.NETLIFY ? '/' : '/line_farm_filter/')
+    : '/',
   plugins: [react()],
-  base: '/line_farm_filter/',
-  server: {
-    fs: {
-      deny: [
-        'src/assets/roles/myenv'
-      ]
-    }
-  }
 })
