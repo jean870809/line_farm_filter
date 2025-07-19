0b9411330b3628fdf97fd4f59e6b3d0272e813f0
Jean <jean87080981@gmail.com>
2025-07-19 19:17:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/line_farm/',
+  base: '/line_farm_filter/',
   server: {
     fs: {
       deny: [
